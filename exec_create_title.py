8cf277a274182c411a40134d8ca120ccb07dda57
ku70t6h1k6r1 <uju5150@gmail.com>
2018-01-25 14:36:18 +0900

    minor fixed
    開発中

--- exec_create_title.py
+++ exec_create_title.py
@@ -1,4 +1,38 @@
+# -*- coding: utf-8 -*-
 import create_title_pos_E as getpos
-
+import random
 
 titlePosObj = getpos.Title()
+
+#繰り返す場合はここから
+getpos.create(titlePosObj)
+titlePos = getpos.create.titlePosA(titlePosObj)
+titleWords = getpos.create.titleA(titlePosObj)
+
+#名詞変換
+meishiIndex = []
+meishiWords = []
+defaultIndex = [0,2]
+
+meishi_x = [random.sample(defaultIndex,1)]
+doushi_x = -1
+
+i = 0
+for pos in titlePos:
+    if pos == '名詞':
+        meishiIndex.append( i )
+    elif pos == 'x_meishi':
+        meishi_x.append( i )
+    elif pos == 'x_doushi':
+        doushi_x = i
+    i += 1
+    
+ for idx in meishiIndex:
+    meishiWords.append(idx)
+    
+ try:
+    #meishi
+    
+ except:
+    
+ 
