87556531545c053de9b2e2b90235c835f3e6b8a2
ku70t6h1k6r1 <uju5150@gmail.com>
2018-01-25 10:27:52 +0900

    Update create_pos_title_E.py
    諸々の修正

--- create_title_pos_E.py
+++ create_title_pos_E.py
@@ -19,6 +19,9 @@ class SqlSet:
             result.append(row)
         return result
 
+    def close(self):
+	self.conn.close()
+
 class Title:
     kigo_a = ['!','?','!?','…']
     word_a = []
@@ -32,7 +35,8 @@ class Title:
     sql = SqlSet()
     result_a = SqlSet.select622(sql)
     result2_a = SqlSet.select107(sql)
-
+    SqlSet.close(sql)
+	
     def create(self):
         tmpTitle = random.sample(self.result_a, 1)
 
@@ -53,29 +57,39 @@ class Title:
             self.title2 = [tmpTitle2[0][2], tmpTitle2[0][3], tmpTitle2[0][4],random.sample(self.kigo_a,1) ]
             self.titlePos2 = [tmpTitle2[0][6], tmpTitle2[0][7], tmpTitle2[0][8], '記号']
         elif tmpTitle[0][11] == 539:
-            self.title2 = [tmpTitle2[0][2], tmpTitle2[0][3], tmpTitle2[0][4], tmpTitle2[0][5], '…']
-            self.titlePos2 = [tmpTitle2[0][6], tmpTitle2[0][7], tmpTitle2[0][8], tmpTitle2[0][9], '記号']
+            self.title2 = [tmpTitle2[0][2], tmpTitle2[0][3], tmpTitle2[0][4], tmpTitle2[0][5], 'x_mesihi']
+            self.titlePos2 = [tmpTitle2[0][6], tmpTitle2[0][7], tmpTitle2[0][8], tmpTitle2[0][9], 'x_mesihi']
         elif tmpTitle[0][11] == 536:
             self.title2 = [tmpTitle2[0][2], tmpTitle2[0][3], tmpTitle2[0][4], tmpTitle2[0][5], '…']
             self.titlePos2 = [tmpTitle2[0][6], tmpTitle2[0][7], tmpTitle2[0][8], tmpTitle2[0][9], '記号']
         elif tmpTitle[0][11] == 535:
-            self.title2 = [tmpTitle2[0][2], tmpTitle2[0][3], tmpTitle2[0][4], tmpTitle2[0][5], '…']
-            self.titlePos2 = [tmpTitle2[0][6], tmpTitle2[0][7], tmpTitle2[0][8], tmpTitle2[0][9], '記号']
+            self.title2 = [tmpTitle2[0][2], tmpTitle2[0][3], tmpTitle2[0][4], tmpTitle2[0][5]]
+            self.titlePos2 = [tmpTitle2[0][6], tmpTitle2[0][7], tmpTitle2[0][8], tmpTitle2[0][9]]
         elif tmpTitle[0][11] == 542:
             self.title2 = [tmpTitle2[0][2], tmpTitle2[0][3], tmpTitle2[0][4], tmpTitle2[0][5], '…']
             self.titlePos2 = [tmpTitle2[0][6], tmpTitle2[0][7], tmpTitle2[0][8], tmpTitle2[0][9], '記号']
         elif tmpTitle[0][11] == 534:
-            self.title2 = [tmpTitle2[0][2], tmpTitle2[0][3], tmpTitle2[0][4], tmpTitle2[0][5], '…']
+            self.title2 = [tmpTitle2[0][2], tmpTitle2[0][3], tmpTitle2[0][4], tmpTitle2[0][5], random.sample(self.kigo_a,1)]
             self.titlePos2 = [tmpTitle2[0][6], tmpTitle2[0][7], tmpTitle2[0][8], tmpTitle2[0][9], '記号']
         elif tmpTitle[0][11] == 544:
             self.title2 = [tmpTitle2[0][2], tmpTitle2[0][3], tmpTitle2[0][4], tmpTitle2[0][5], '…']
             self.titlePos2 = [tmpTitle2[0][6], tmpTitle2[0][7], tmpTitle2[0][8], tmpTitle2[0][9], '記号']
         elif tmpTitle[0][11] == 538:
-            self.title2 = [tmpTitle2[0][2], tmpTitle2[0][3], tmpTitle2[0][4], tmpTitle2[0][5], '…']
-            self.titlePos2 = [tmpTitle2[0][6], tmpTitle2[0][7], tmpTitle2[0][8], tmpTitle2[0][9], '記号']
+            self.title2 = [tmpTitle2[0][2], tmpTitle2[0][3], tmpTitle2[0][4], 'x_meishi']
+            self.titlePos2 = [tmpTitle2[0][6], tmpTitle2[0][7], tmpTitle2[0][8], 'x_meishi']
         else:
-            self.title2 = [tmpTitle2[0][2], tmpTitle2[0][3], tmpTitle2[0][4], tmpTitle2[0][5], '…']
-            self.titlePos2 = [tmpTitle2[0][6], tmpTitle2[0][7], tmpTitle2[0][8], tmpTitle2[0][9], '記号']
+            self.title2 = [tmpTitle2[0][2], tmpTitle2[0][3], tmpTitle2[0][4], tmpTitle2[0][5]]
+            self.titlePos2 = [tmpTitle2[0][6], tmpTitle2[0][7], tmpTitle2[0][8], tmpTitle2[0][9]]
+    def title(self):
+	output = '/'.join(self.title) + '/ /' + '/'.join(self.title2)
+	return output
 
-test = Title()
-print Title.create(test)
+    def titlePos(self):
+	output = '/'.join(self.titlePos) + '/ /' + '/'.join(self.titlePos2)
+	return output
+
+　　def titlePosA(self):
+	return self.titlePos.extend([' ']).extend(self.titlePos2)
+
+   def titleA(self):
+	return self.title.extend([' ']).extend(self.title2)
