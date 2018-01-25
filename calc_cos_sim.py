c0c6f0c53d420d423d74a1514ac7f9063b8b9490
ku70t6h1k6r1 <uju5150@gmail.com>
2018-01-26 01:42:43 +0900

    fixed for python

--- calc_cos_sim.py
+++ calc_cos_sim.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 import sqlite3
 import random
+import numpy as np
+import pickle
 
 #数値ベクトルはnumpy
 #言葉は配列
@@ -9,32 +11,84 @@ import random
 class RelatedWords:
     dbname = 'word2vec.db'
     conn = sqlite3.connect(dbname)
+    conn.text_factory = str
     c = conn.cursor()
-    
+
+    with open('meishi_vec.pickle', 'rb') as f:
+        wsVec = pickle.load(f)    
+
+    with open('doushi_vec.pickle', 'rb') as f2:
+        wsVec2 = pickle.load(f2)
+
+    with open('meishi_words.pickle', 'rb') as f3:
+        words_a = pickle.load(f3)
+
+    with open('doushi_words.pickle', 'rb') as f4:
+        words2_a = pickle.load(f4)
+
+
     def get(self, wi, outn):
-        wiScore = 
-    end
-    
-    def get_FromVec(self, wsVec, words_a, vec, outn):
+        wiScore = self.wsVec[self.words_a.index(wi),:] * self.wsVec.T
+	wiScoreA = np.ravel(wiScore)
+
+        output_w = []
+        output_score = []
+
+        for i in range(outn):
+            output_w.append(self.words_a[np.argsort(wiScoreA)[::-1][i]])
+            output_score.append(np.sort(wiScoreA)[::-1][i])
+
+        output = {"words":output_w, "scores":output_score}
+	return output
+
+
+    #def get_FromVec(self, wsVec, words_a, vec, outn):
         # main issue
-    end
     
-    def subtract(self, wi1_a, wi2_a, w1_a, w2_a):
-    
-        i = 0
-        for wi1 in wi1_a:
+    #def subtract(self, wi1_a, wi2_a, w1_a, w2_a):
+    #    i = 0
+    #    for wi1 in wi1_a:
      
-    def add(self, wi_a, weight_a):
-        i = 0
-           for wi in wi_a:
+    #def add(self, wi_a, weight_a):
+    #    i = 0
+    #       for wi in wi_a:
     
-    def add_vec(self, wi, vec, weight):
+    #def add_vec(self, wi, vec, weight):
     
-    def add_vecvec(self, vec1, vec2):
+    #def add_vecvec(self, vec1, vec2):
     
-    def wi2vec(self, wi):
+    #def wi2vec(self, wi):
     
-    def add_noize(self, vec):
-    
+    #def add_noize(self, vec):
     def indexToWord(self, wi):
+        result = []
+        for row in self.c.execute('select word from articles_vocab_control where word_id == {0}'.format(wi)):
+            result.append(row[0])
+        return result[0]
         
+    def wordToIndex(self, w):
+        result = []
+        for row in self.c.execute('select word_id from articles_vocab_control where word == \'{0}\''.format(w)):
+            result.append(row[0])
+        return result[0]
+
+    def indexToWords(self, wi_a):
+        words = []
+        for wi in wi_a:
+            result = []
+            for row in self.c.execute('select word from articles_vocab_control where word_id == {0}'.format(wi)):
+                result.append(row[0])
+            words.append(result[0])
+        print words
+        return words
+
+    def close(self):
+	self.conn.close()
+
+test = RelatedWords()
+wi = RelatedWords.wordToIndex(test, '恋愛')
+result = RelatedWords.get(test, wi, 10)
+print str(RelatedWords.indexToWords(test, result["words"])).decode('string-escape')
+
+RelatedWords.close(test)
+
--- create_title_pos_E.py
+++ create_title_pos_E.py
@@ -1,10 +1,12 @@
-# -*- coding: utf-8 -*-
+# coding: UTF-8
+
 import sqlite3
 import random
 
 class SqlSet:
     dbname = 'word2vec.db'
     conn = sqlite3.connect(dbname)
+    conn.text_factory = str  
     c = conn.cursor()
 
     def select107(self):
@@ -44,52 +46,57 @@ class Title:
            self.title =  [tmpTitle[0][2], tmpTitle[0][3], tmpTitle[0][4], tmpTitle[0][5], '…']
            self.titlePos =  [tmpTitle[0][6], tmpTitle[0][7], tmpTitle[0][8], tmpTitle[0][9], '記号']
         else:
-           self.title = [tmpTitle[0][2], tmpTitle[0][3], tmpTitle[0][4],random.sample(self.kigo_a,1)]
+           self.title = [tmpTitle[0][2], tmpTitle[0][3], tmpTitle[0][4],random.sample(self.kigo_a,1)[0]]
            self.titlePos = [tmpTitle[0][6], tmpTitle[0][7], tmpTitle[0][8],'記号']
-        return self.title
 
         tmpTitle2 = random.sample(self.result2_a, 1)
-
-        if tmpTitle[0][11] == 537:
+	
+        if tmpTitle2[0][11] == 537:
             self.title2 = [tmpTitle2[0][2], tmpTitle2[0][3], tmpTitle2[0][4], 'x_doushi']
             self.titlePos2 = [tmpTitle2[0][6], tmpTitle2[0][7], tmpTitle2[0][8], 'x_doushi']
-        elif tmpTitle[0][11] == 543:
-            self.title2 = [tmpTitle2[0][2], tmpTitle2[0][3], tmpTitle2[0][4],random.sample(self.kigo_a,1) ]
+        elif tmpTitle2[0][11] == 543:
+            self.title2 = [tmpTitle2[0][2], tmpTitle2[0][3], tmpTitle2[0][4],random.sample(self.kigo_a,1)[0] ]
             self.titlePos2 = [tmpTitle2[0][6], tmpTitle2[0][7], tmpTitle2[0][8], '記号']
-        elif tmpTitle[0][11] == 539:
+        elif tmpTitle2[0][11] == 539:
             self.title2 = [tmpTitle2[0][2], tmpTitle2[0][3], tmpTitle2[0][4], tmpTitle2[0][5], 'x_mesihi']
             self.titlePos2 = [tmpTitle2[0][6], tmpTitle2[0][7], tmpTitle2[0][8], tmpTitle2[0][9], 'x_mesihi']
-        elif tmpTitle[0][11] == 536:
+        elif tmpTitle2[0][11] == 536:
             self.title2 = [tmpTitle2[0][2], tmpTitle2[0][3], tmpTitle2[0][4], tmpTitle2[0][5], '…']
             self.titlePos2 = [tmpTitle2[0][6], tmpTitle2[0][7], tmpTitle2[0][8], tmpTitle2[0][9], '記号']
-        elif tmpTitle[0][11] == 535:
+        elif tmpTitle2[0][11] == 535:
             self.title2 = [tmpTitle2[0][2], tmpTitle2[0][3], tmpTitle2[0][4], tmpTitle2[0][5]]
             self.titlePos2 = [tmpTitle2[0][6], tmpTitle2[0][7], tmpTitle2[0][8], tmpTitle2[0][9]]
-        elif tmpTitle[0][11] == 542:
+        elif tmpTitle2[0][11] == 542:
             self.title2 = [tmpTitle2[0][2], tmpTitle2[0][3], tmpTitle2[0][4], tmpTitle2[0][5], '…']
             self.titlePos2 = [tmpTitle2[0][6], tmpTitle2[0][7], tmpTitle2[0][8], tmpTitle2[0][9], '記号']
-        elif tmpTitle[0][11] == 534:
-            self.title2 = [tmpTitle2[0][2], tmpTitle2[0][3], tmpTitle2[0][4], tmpTitle2[0][5], random.sample(self.kigo_a,1)]
+        elif tmpTitle2[0][11] == 534:
+            self.title2 = [tmpTitle2[0][2], tmpTitle2[0][3], tmpTitle2[0][4], tmpTitle2[0][5], random.sample(self.kigo_a,1)[0]]
             self.titlePos2 = [tmpTitle2[0][6], tmpTitle2[0][7], tmpTitle2[0][8], tmpTitle2[0][9], '記号']
-        elif tmpTitle[0][11] == 544:
+        elif tmpTitle2[0][11] == 544:
             self.title2 = [tmpTitle2[0][2], tmpTitle2[0][3], tmpTitle2[0][4], tmpTitle2[0][5], '…']
             self.titlePos2 = [tmpTitle2[0][6], tmpTitle2[0][7], tmpTitle2[0][8], tmpTitle2[0][9], '記号']
-        elif tmpTitle[0][11] == 538:
+        elif tmpTitle2[0][11] == 538:
             self.title2 = [tmpTitle2[0][2], tmpTitle2[0][3], tmpTitle2[0][4], 'x_meishi']
             self.titlePos2 = [tmpTitle2[0][6], tmpTitle2[0][7], tmpTitle2[0][8], 'x_meishi']
         else:
             self.title2 = [tmpTitle2[0][2], tmpTitle2[0][3], tmpTitle2[0][4], tmpTitle2[0][5]]
             self.titlePos2 = [tmpTitle2[0][6], tmpTitle2[0][7], tmpTitle2[0][8], tmpTitle2[0][9]]
+
     def title(self):
-	output = '/'.join(self.title) + '/ /' + '/'.join(self.title2)
+	output = '/'.join(self.title) + '/ /' +  '/'.join(self.title2)
 	return output
 
     def titlePos(self):
 	output = '/'.join(self.titlePos) + '/ /' + '/'.join(self.titlePos2)
 	return output
 
-　　def titlePosA(self):
+    def titlePosA(self):
 	return self.titlePos.extend([' ']).extend(self.titlePos2)
 
-   def titleA(self):
+    def titleA(self):
 	return self.title.extend([' ']).extend(self.title2)
+
+#test = Title()
+#Title.create(test)
+#print Title.title(test)
+#print Title.titlePos(test)
