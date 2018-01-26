3dc06e90e9c729c155dd79aca800b06878ab9a5d
ku70t6h1k6r1 <uju5150@gmail.com>
2018-01-26 23:56:43 +0900

    Update exec file
    minor update

--- exec_create_title.py
+++ exec_create_title.py
@@ -1,38 +1,57 @@
+#!/usr/lib/python2.7
 # -*- coding: utf-8 -*-
 import create_title_pos_E as getpos
+import calc_cos_sim　as getwords
 import random
+
+word = ['恋愛','変態']
+
+relatedWords = getwords.RelatedWords()
+word_id = getwords.RelatedWords.wordsToIndex(relatedWords, word)
 
 titlePosObj = getpos.Title()
 
-#繰り返す場合はここから
-getpos.create(titlePosObj)
-titlePos = getpos.create.titlePosA(titlePosObj)
-titleWords = getpos.create.titleA(titlePosObj)
+outTitles = {}
+for i in range(20):
+    getpos.Title.create(titlePosObj)
+    titlePos = getpos.Title.titlePosA(titlePosObj)
+    titleWords = getpos.Title.titleA(titlePosObj)
 
-#名詞変換
-meishiIndex = []
-meishiWords = []
-defaultIndex = [0,2]
+    #名詞変換
+    meishiIndex = []
+    meishiWords = []
+    defaultIndex = [0,2]
 
-meishi_x = [random.sample(defaultIndex,1)]
-doushi_x = -1
+    meishi_x = random.sample(defaultIndex,1)
+    doushi_x = -1
 
-i = 0
-for pos in titlePos:
-    if pos == '名詞':
-        meishiIndex.append( i )
-    elif pos == 'x_meishi':
-        meishi_x.append( i )
-    elif pos == 'x_doushi':
-        doushi_x = i
-    i += 1
+    i = 0
+    for pos in titlePos:
+        if pos == '名詞':
+            meishiIndex.append( i )
+        elif pos == 'x_meishi':
+            meishi_x.append( i )
+        elif pos == 'x_doushi':
+            doushi_x = i
+        i += 1
     
- for idx in meishiIndex:
-    meishiWords.append(idx)
+    for idx in meishiIndex:
+        meishiWords.append(titleWords[idx])
     
- try:
-    #meishi
-    
- except:
+    try:
+        #meishi
+        resultWord = getwords.relatedWords.getWords(meishiWords, word, 'meishi', len(meishi_x))
+        
+        j = 0
+        for idx in meishi_x:
+            titleWords[idx] = resultWord[j]
+            j += 1
+         
+        if doushi_x > -1:
+            resultWord2 = getwords.relatedWords.getWords(meishiWords, word, 'doushi', 1)
+            titleWords[doushi_x] = resultWord2[0] 
+            print "#############" #str(RelatedWords.indexToWords(test, result["words"])).decode('string-escape')
+            print titleWords
+    except:
     
  
