6e8dd3e32467a8f8232c339ca1f4e325ec5bb4ca
ku70t6h1k6r1 <uju5150@gmail.com>
2018-01-26 22:41:10 +0900

    fixed calc_cos_sim.py

--- calc_cos_sim.py
+++ calc_cos_sim.py
@@ -6,9 +6,9 @@ import pickle
 
 def normalize(v, axis=-1, order=2):
     l2 = np.linalg.norm(v, ord = order, axis=axis, keepdims=True)
-    if l2==0:
-        l2 = 1
-    return v/l2
+    if l2[0]==0:
+        l2[0] = 1 
+    return v/l2[0]
 
 #数値ベクトルはnumpy
 #言葉は配列
@@ -23,7 +23,6 @@ class RelatedWords:
     with open('meishi_vec.pickle', 'rb') as f:
         wsVec = pickle.load(f)    
     vec_size = wsVec.shape[1]
-    print vec_size
     
     with open('doushi_vec.pickle', 'rb') as f2:
         wsVec2 = pickle.load(f2)
@@ -37,7 +36,7 @@ class RelatedWords:
 
     def get(self, wi, outn):
         wiScore = self.wsVec[self.words_a.index(wi),:] * self.wsVec.T
-	    wiScoreA = np.ravel(wiScore)
+	wiScoreA = np.ravel(wiScore)
 
         output_w = []
         output_score = []
@@ -47,21 +46,21 @@ class RelatedWords:
             output_score.append(np.sort(wiScoreA)[::-1][i])
 
         output = {"words":output_w, "scores":output_score}
-	    return output
+	return output
 
     def get_FromVec(self, wsVec, words_a, vec, outn):
-        wiScore = vec * wsVec.T #vecのフォーマットはnumpy.array
-	    wiScoreA = np.ravel(wiScore)
+        wiScore = np.matrix(vec[0,:]) * wsVec.T
+        wiScoreA = np.ravel(wiScore)
 
         output_w = []
         output_score = []
         
         for i in range(outn):
-            output_w.append(self.words_a[np.argsort(wiScoreA)[::-1][i]])
+            output_w.append(words_a[np.argsort(wiScoreA)[::-1][i]])
             output_score.append(np.sort(wiScoreA)[::-1][i])
 
         output = {"words":output_w, "scores":output_score}
-	    return output        
+	return output        
     
     def subtract(self, wi1_a, wi2_a, w1_a, w2_a):        
         vec_p = np.zeros(self.vec_size)
@@ -69,8 +68,8 @@ class RelatedWords:
         
         i = 0
         for wi1 in wi1_a :
-            vec_p  = vec_p + w1_a[i] * np.ravel(self.wsVec[self.words_a.index(wi1),:]) 
-            i += 1
+           vec_p  = vec_p + w1_a[i] * np.ravel(self.wsVec[self.words_a.index(wi1),:]) 
+           i += 1
         
         i = 0
         for wi2 in wi2_a :
@@ -85,10 +84,10 @@ class RelatedWords:
         vec = np.zeros(self.vec_size)
       
         i = 0
-        for wi in wi_a :
+        for wi in wi_a:
             vec  = vec + weight_a[i] * np.ravel(self.wsVec[self.words_a.index(wi),:]) 
             i += 1
-            
+
         vec_norm = normalize(vec)
         return vec_norm
 
@@ -138,16 +137,17 @@ class RelatedWords:
     def close(self):
 	    self.conn.close()
     
-    def getWords(self, preWs_a, inputWs_a, pos, n)
-        preWs_a = self.WordsToIndex(preWs_a)
+    def getWords(self, preWs_a, inputWs_a, pos, n):
+        preWs_a = self.wordsToIndex(preWs_a)
         inputWs_a = self.wordsToIndex(inputWs_a)
         inputWsVec_a = self.add(inputWs_a, np.ones(len(inputWs_a)))
-        
+
 	preW = np.repeat([0.2], len(preWs_a))
         inW = np.ones(len(inputWs_a))
         weight_a = np.r_[preW, inW]
-        sumWs = preWs_a.extend(inputWs_a)
-        
+
+        preWs_a.extend(inputWs_a)
+        sumWs = preWs_a
         addVec = self.add(sumWs, weight_a)
         outputVec = self.add_vecvec( addVec, self.add_noize(inputWsVec_a) )
         
@@ -159,12 +159,15 @@ class RelatedWords:
             print 'sorry!'
 
         outputWords = outputWords['words']
-        return self.indexToWprds(random.sample(outputWords, n))
+        return self.indexToWords(random.sample(outputWords, n))
     
 test = RelatedWords()
+print "VECTRISED !!"
 wi = RelatedWords.wordToIndex(test, '恋愛')
 result = RelatedWords.get(test, wi, 10)
-print str(RelatedWords.indexToWords(test, result["words"])).decode('string-escape')
+#print str(RelatedWords.indexToWords(test, result["words"])).decode('string-escape')
+
+print str(RelatedWords.getWords(test, ['犬', 'ペンギン', '時計'], ['恋愛', '女' ], 'meishi', 10)).decode('string-escape')
 
 RelatedWords.close(test)
 
