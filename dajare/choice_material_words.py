563224a689bf0b62421bb9a7c19cb24a9fcb5517
ku70t6h1k6r1 <uju5150@gmail.com>
2018-04-05 12:44:14 +0900

    add dajare module

--- dajare/choice_material_words.py
+++ dajare/choice_material_words.py
@@ -1,1 +1,39 @@
+# -*- coding: utf-8 -*-
+import sqlite3
+import random
+import numpy as np
+import MeCab
+
+class Sql:
+    dbname = '../word2vec.db'
+    conn = sqlite3.connect(dbname)
+    conn.text_factory = str
+    c = conn.cursor()
+
+    def select(self, get_words_n):
+        wi_a = np.random.randint(0,100000,get_words_n)
+        result = []
+        for wi in wi_a:
+                for row in self.c.execute('select word from wiki_plus_articles_vocab_control where word_index == {0}'.format(wi)):
+                        result.append(row[0])
+
+        return result
+
+if __name__ == '__main__':
+	sql = Sql()
+	results = sql.select(40)
+	for w in results:
+		print(w)
+
+test_sentence = '犬山dtm'
+
+    
+
+
+def test04(s):
+    tagger = MeCab.Tagger('-Oyomi')
+    result = tagger.parse(s)
+    print result
+    
+test04(test_sentence)
 
